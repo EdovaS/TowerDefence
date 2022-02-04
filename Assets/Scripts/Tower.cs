d3a44476f16d0c5d355d7199d9a0c2f3a288d8ef
EdovaS <98994696+EdovaS@users.noreply.github.com>
2022-02-04 07:42:32 +0530

    added Editor Console pro

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    
+    
+    
 }
